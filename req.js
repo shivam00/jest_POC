c364303564d1d669617e996dabb64ea45198255f
shivam gupta <shivam.gupta0002@gmail.com>
2018-12-09 06:18:33 -1100

    Update req.js

--- req.js
+++ req.js
@@ -1,11 +1,6 @@
 import axios from "axios";
-
-
 export default async () =>
 {
-
-
 const response = await axios.get("https://api.github.com/users/shivam00",{ crossdomain: true });
-
 return response;
 };
